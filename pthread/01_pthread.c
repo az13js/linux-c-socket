7c0fb1fe4e8d0187fc1b6975a7f2d5a8e7999a49
az13js <1654602334@qq.com>
2020-07-01 23:39:35 +0800

    Test

---
+++ pthread/01_pthread.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <pthread.h>
+
+void print_message() {
+    printf("线程\n");
+}
+
+int main(int argc, char *argv[]) {
+    pthread_t t;
+    pthread_create();
+    return 0;
+}
+
