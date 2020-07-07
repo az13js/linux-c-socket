0d0c7354a5daf9890c29062a3ef74afb788f2b84
az13js <1654602334@qq.com>
2020-07-07 22:58:02 +0800

    临时保存

--- pthread/06_thread_attr_join.c
+++ pthread/06_thread_attr_join.c
@@ -4,13 +4,11 @@
 #include <stdlib.h>
 
 void *thread_function (void *arg) {
-    printf("Thread function is running\n");
+    printf("线程正在执行\n");
     sleep(1);
-    printf("Thread function is running\n");
+    printf("线程正在执行\n");
     sleep(1);
-    printf("Thread function is running\n");
-    sleep(1);
-    printf("Thread function end\n");
+    printf("线程正在执行\n");
 }
 
 int main(void) {
@@ -19,32 +17,32 @@ int main(void) {
 
     pthread_attr_t attr;
     if (0 != pthread_attr_init(&attr)) {
-        printf("Init fail\n");
+        printf("属性设置初始化失败\n");
         return 0;
     }
     if (0 != pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE)) {
-        printf("Set Detach state fail\n");
+        printf("设置可结合状态失败\n");
         return 0;
     }
     result = pthread_create(&thread, &attr, &thread_function, NULL);
     if (0 != result) {
-        printf("Create thread fail!\n");
+        printf("创建线程失败\n");
         return 0;
     }
-    printf("Thread creae ok.\n");
+    printf("线程创建成功\n");
     if (0 != pthread_join(thread, NULL)) {
-        printf("Join error\n");
+        printf("pthread_join()调用出现异常\n");
         return 0;
     } else {
-        printf("Join success\n");
+        printf("pthread_join()调用完成\n");
     }
-    printf("Main function is running\n");
+    printf("进程正在运行\n");
     sleep(1);
-    printf("Main function is running\n");
+    printf("进程正在运行\n");
     sleep(1);
-    printf("Main function is running\n");
+    printf("进程正在运行\n");
     sleep(1);
-    printf("Main function end\n");
+    printf("进程执行完成，即将退出\n");
     return 0;
 }
 
