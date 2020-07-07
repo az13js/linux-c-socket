03b2b9bb1d0171adfd2748daddfa7718b28788ff
az13js <1654602334@qq.com>
2020-07-07 19:15:11 +0800

    两种分离方式的测试

---
+++ pthread/06_thread_attr_join.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void *thread_function (void *arg) {
+    printf("Thread function is running\n");
+    sleep(1);
+    printf("Thread function is running\n");
+    sleep(1);
+    printf("Thread function is running\n");
+    sleep(1);
+    printf("Thread function end\n");
+}
+
+int main(void) {
+    pthread_t thread;
+    int result;
+
+    pthread_attr_t attr;
+    if (0 != pthread_attr_init(&attr)) {
+        printf("Init fail\n");
+        return 0;
+    }
+    if (0 != pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE)) {
+        printf("Set Detach state fail\n");
+        return 0;
+    }
+    result = pthread_create(&thread, &attr, &thread_function, NULL);
+    if (0 != result) {
+        printf("Create thread fail!\n");
+        return 0;
+    }
+    printf("Thread creae ok.\n");
+    if (0 != pthread_join(thread, NULL)) {
+        printf("Join error\n");
+        return 0;
+    } else {
+        printf("Join success\n");
+    }
+    printf("Main function is running\n");
+    sleep(1);
+    printf("Main function is running\n");
+    sleep(1);
+    printf("Main function is running\n");
+    sleep(1);
+    printf("Main function end\n");
+    return 0;
+}
+
